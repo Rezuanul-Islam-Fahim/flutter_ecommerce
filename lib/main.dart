b5109b104db2d7b470ba249398c226881169d9f6
Rezuanul Islam Fahim <rifahim98@gmail.com>
2022-09-16 23:39:42 +0600

    Initialize firebase before app starts

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(const FlutterEcommerce());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const FlutterEcommerce());
+}
 
 class FlutterEcommerce extends StatelessWidget {
   const FlutterEcommerce({Key? key}) : super(key: key);
